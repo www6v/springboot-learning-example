5299e59bc74f61fde3a2960d56c6e5ff88c8784c
bruce.wang <www6v@126.com>
2019-11-04 11:07:55 +0800

    fix

--- springboot-restful/src/main/java/org/spring/springboot/logDomain/operation/OpertionData.java
+++ springboot-restful/src/main/java/org/spring/springboot/logDomain/operation/OpertionData.java
@@ -3,7 +3,7 @@ package org.spring.springboot.logDomain.operation;
 import org.hibernate.validator.constraints.Range;
 
 public class OpertionData {
-    @Range(min = 1, max = 10, message = "range在[1,10]之间")
+    @Range(min = 1, max = 13, message = "range在[1,13]之间")
     private int opertionType;
 
     public int getOpertionType() {
